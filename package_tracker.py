cf16b6e0520b655d0558ddcb52ea90e1bf5c2f6f
alec <alecbgarcia@gmail.com>
2020-10-15 10:57:34 -0700

    package to database successful

--- package_tracker.py
+++ package_tracker.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, redirect
 from config import Config
 from app.shipping_form import ShippingForm
+from flask_migrate import Migrate
+from app.models import db, Package
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -13,5 +17,13 @@ def index():
 def new_package():
     form = ShippingForm()
     if form.validate_on_submit():
+        data = form.data
+        new_package = Package(sender=data["sender"],
+                              recipient=data["recipient"],
+                              origin=data["origin"],
+                              destination=data["destination"],
+                              location=data["origin"])
+        db.session.add(new_package)
+        db.session.commit()
         return redirect('/')
     return render_template('shipping_request.html', form=form)
